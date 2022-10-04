c5a5f9303d0f65caa886fd57ec3f5ff8f7935912
Chrispy98 <45731625+Chrispy98@users.noreply.github.com>
2022-10-04 08:58:48 +0200

    Add files via upload

---
+++ VenVen/ven.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            echo "Ven, ven lejos de los malos suenos<br>
+            Donde nuestro amor no sienta miedo<br>
+            Volaremos juuuntos sin temer la oscuridad<br>
+            Tu y yooooooooooo<br>
+            Miiiles de kilometros sin ti<br>
+            Nuestro camino se cruza al fin<br>
+            Quisiera ser<br>
+            Un nino que olvido crecer<br>
+            Para estar<br>
+            Jugando contiiigo una vez mas<br>
+            Robando las flores de tu jardin<br>
+            Y decirte<br>
+            Las cosas que nooo supe decir<br>
+            Cruzaremos la luz<br>
+            Que ira<br>
+            Iluminando un mundo que parezca pequeño<br>
+            Al lado de nuestra voz<br>
+            Con la ilusion de vooolver<br>
+            A ser como aayeeeeeer<br>
+            Ven, ven lejos de los malos suenos<br>
+            Donde nuestro amor no sienta miedo<br>
+            Volaremos juuuntos sin temer la oscuridad<br>
+            Tu y yooooooooooooooooooooooooo<br>";
+        ?>
+    </body>
+</html>
