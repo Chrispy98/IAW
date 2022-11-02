cfb3df0a26c4c157ece1ea26ec204fc5c6ef997a
Chrispy98 <45731625+Chrispy98@users.noreply.github.com>
2022-11-02 08:58:27 +0100

    Add files via upload

--- Practica1/practica1.sql
+++ Practica1/practica1.sql
@@ -1,18 +1,98 @@
-CREATE TABLE cliente (
-     dni VARCHAR PRIMARY KEY NOT NULL,
-     nombre VARCHAR,
-     cuenta_bancaria VARCHAR);
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-11-2022 a las 08:57:27
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.2.33
 
-CREATE TABLE flores (
-     idflor INT PRIMARY KEY NOT NULL,
-     precio INT,
-     cantidad INT NOT NULL);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE compras (
-     dni VARCHAR,
-     idflor INT,
-     fecha DATE,
-     cantidad INT, 
-     PRIMARY KEY (dni, idflor), 
-     FOREIGN KEY (dni) REFERENCES cliente(dni), 
-     FOREIGN KEY (idflor) REFERENCES flores(idflor));
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `practica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `dni` varchar(15) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `cuenta_bancaria` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `dni` varchar(15) NOT NULL,
+  `idflor` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `flores`
+--
+
+CREATE TABLE `flores` (
+  `idflor` int(11) NOT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`dni`,`idflor`),
+  ADD KEY `idflor` (`idflor`);
+
+--
+-- Indices de la tabla `flores`
+--
+ALTER TABLE `flores`
+  ADD PRIMARY KEY (`idflor`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `cliente` (`dni`),
+  ADD CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`idflor`) REFERENCES `flores` (`idflor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
