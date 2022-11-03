4ac1fe60478a81e21ac09e31341a4c76fb84b920
Chrispy98 <45731625+Chrispy98@users.noreply.github.com>
2022-11-03 14:20:24 +0100

    Add files via upload

--- Practica1/compras.php
+++ Practica1/compras.php
@@ -14,17 +14,77 @@
             }
         </script>
     </head>
+    <?php
+        if (isset($_REQUEST['alta'])){
+            $boton=0;
+        } else if (isset($_REQUEST['extracto'])){
+            $boton=1;
+        }
+            
+    ?>
     <body>
         <a href="principal.html">Home</a>
         <a href="almacen.html">Almacén</a>
         <a href="clientes.html">Clientes</a>
         <a href="compras.html">Compras</a><br><br>
         <form action="" method="post">
-            NIF: <input type="text" name="nif"><br><br>
+            NIF: <input type="text" name="dni"><br><br>
             ID: <input type="number" name="idflor"><br><br>
             Cantidad: <input type="number" name="cantidad"><br><br>
             <input type="submit" name="alta" value="Alta">
             <input type="submit" name="extracto" value="Extracto de compras">
         </form>
     </body>
+    <?php
+        include 'funcionesFloristeria.php';
+        if (isset($boton)) {
+            if ($boton==0){
+                if (isset($_REQUEST['dni']) && $_REQUEST['dni']!="" 
+                && isset($_REQUEST['idflor']) && $_REQUEST['idflor']!=""
+                && isset($_REQUEST['cantidad']) && $_REQUEST['cantidad']!="") {
+                    if (hayStock($_REQUEST['idflor'], $_REQUEST['cantidad'])) {
+                        if (sePuedeComprar($_REQUEST['idflor'])){
+                            $conexion=mysqli_connect("localhost", "root", "", "practica") 
+                                 or die("Problemas en la conexión");
+                            $insert="INSERT INTO compras VALUES ('$_REQUEST[dni]', $_REQUEST[idflor], NOW(), $_REQUEST[cantidad])";
+                            mysqli_query($conexion,$insert)
+                                or die ("Problemas en el insert: ".mysqli_error());
+                            mysqli_close($conexion);
+                            $conexion=mysqli_connect("localhost", "root", "", "practica") 
+                                 or die("Problemas en la conexión");
+                            $update="UPDATE flores SET cantidad=cantidad-$_REQUEST[cantidad] WHERE idFlor=$_REQUEST[idflor]";
+                            mysqli_query($conexion,$update)
+                                or die ("Problemas en el update: ".mysqli_error());
+                            mysqli_close($conexion);
+                            echo "Compra realizada correctamente";
+                        }
+                        else {
+                            echo "Espérese unos instantes para comprar esta flor. Perdona las molestias.";
+                        }
+                    }
+                    else {
+                        echo "No hay suficiente stock para la cantidad deseada";
+                    }
+                }
+                else {
+                    echo "Tienes que completar todos los campos";
+                }
+            } else if ($boton==1){
+                $conexion=mysqli_connect("localhost","root","","practica")
+                        or die("Problemas de conexión");
+                $select="SELECT idflor, cantidad, dni, date_format(fecha, '%d-%m-%Y %H:%i:%s') AS fec FROM compras;";
+                $filas=mysqli_query($conexion,$select) 
+                    or die("Problemas en el select: ".mysqli_error($conexion));
+                if ($filas) {
+                    $fila=mysqli_fetch_array($filas);
+                    while ($fila) {
+                        echo "IDFlor: ".$fila['idflor']." | DNI: ".$fila['dni']." | Cantidad: ".$fila['cantidad'].
+                            "| Fecha: ".$fila['fec']."<br>";
+                        $fila=mysqli_fetch_array($filas);
+                    }
+                }
+                mysqli_close($conexion);
+            }
+        }
+    ?>
 </html>
--- Practica1/funcionesFloristeria.php
+++ Practica1/funcionesFloristeria.php
@@ -36,4 +36,42 @@
             }
         }
     }
+
+    function hayStock($id, $canti){
+        $conexion=mysqli_connect("localhost", "root", "", "practica") 
+            or die("Problemas en la conexión");
+        $select="SELECT cantidad FROM flores WHERE idFlor=$id;";
+        $filas=mysqli_query($conexion,$select)
+            or die("Problemas con el select: ".mysqli_error($conexion));
+        if ($filas){
+            $fila=mysqli_fetch_array($filas);
+            if ($canti>$fila['cantidad']){
+                mysqli_close($conexion);
+                return false;
+            }
+            else {
+                mysqli_close($conexion);
+                return true;
+            }
+        }
+    }
+
+    function sePuedeComprar($id){
+        $conexion=mysqli_connect("localhost", "root", "", "practica") 
+            or die("Problemas en la conexión");
+        $select="SELECT COUNT(*) AS cont FROM compras WHERE idFlor=$idFlor AND fecha=NOW()";
+        $filas=mysqli_query($conexion,$select)
+            or die("Problemas con el select: ".mysqli_error($conexion));
+        if ($filas){
+            $fila=mysqli_fetch_array($filas);
+            if ($fila['cont']==0){
+                mysqli_close($conexion);
+                return true;
+            }
+            else {
+                mysqli_close($conexion);
+                return false;
+            }
+        }
+    }
 ?>
